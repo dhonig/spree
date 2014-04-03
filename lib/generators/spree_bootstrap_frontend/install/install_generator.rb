a88f401e590a5e5ae0c78b18b1c0f9335e832ae3
Jeff Dutil <jdutil@burlingtonwebapps.com>
2014-04-03 11:54:18 +0100

    Merged changes from https://github.com/JDutil/spree_bootstrap_frontend.git into master.
    Changed from #49. This commit only includes the functional changes and not the specs. I'll add the specs in a new commit.

--- lib/generators/spree_bootstrap_frontend/install/install_generator.rb
+++ lib/generators/spree_bootstrap_frontend/install/install_generator.rb
@@ -1,12 +1,19 @@
 module SpreeBootstrapFrontend
   module Generators
     class InstallGenerator < Rails::Generators::Base
+
       source_root File.expand_path("../templates", __FILE__)
 
-      def copy_stylesheets
-        copy_file 'stylesheets/spree_bootstrap_frontend.css.scss',
-                  'app/assets/stylesheets/spree/frontend/spree_bootstrap_frontend.css.scss'
+      def add_javascripts
       end
+
+      def add_stylesheets
+        copy_file 'stylesheets/spree_bootstrap_frontend.css.scss',
+                  'vendor/assets/stylesheets/spree/frontend/spree_bootstrap_frontend.css.scss'
+        copy_file 'stylesheets/all.css',
+                  'vendor/assets/stylesheets/spree/frontend/all.css'
+      end
+
     end
   end
 end
