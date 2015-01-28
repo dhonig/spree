a21d962b37e3b4a37f35228993c2531d909d468a
John Hawthorn <john.hawthorn@gmail.com>
2015-01-28 00:46:32 -0800

    Remove preferences configuration from admin
    Remove Spree::Money preferences
    
    This hurt performance since each instantiation of a Spree::Money object
    required 6 preference (and thus cache) lookups.
    
    RubyMoney does most of this better by default:
    
    * The decimal mark and thousands separator are taken from i18n, allowing
      multilingual sites to now format their moneys correctly.
    * symbol_first is configured by RubyMoney per-currency, this allows
      multi-currency sites to now correctly format their currencies. This is
      also better for new single currency sites, as there is no longer a
      need to configure this.
    
    sign_before_symbol was the one exception, where our default was
    different (I would argue better) than the RubyMoney team's. So I have
    added Spree::Money.default_formatting_rules, which is analogous to
    Money..default_formatting_rules which has a default of
    {sign_before_symbol: true}.
    
    This Spree::Money.default_formatting_rules can also be used in place of
    the existing preference values.
    
    Fixes #5966

--- backend/app/controllers/spree/admin/general_settings_controller.rb
+++ backend/app/controllers/spree/admin/general_settings_controller.rb
@@ -6,7 +6,7 @@ module Spree
       before_action :set_store
 
       def edit
-        @preferences_security = []
+        @preferences_security = [:check_for_spree_alerts]
       end
 
       def update
@@ -17,8 +17,17 @@ module Spree
 
         current_store.update_attributes store_params
 
-        flash[:success] = Spree.t(:successfully_updated, resource: Spree.t(:general_settings))
+        flash[:success] = Spree.t(:successfully_updated, :resource => Spree.t(:general_settings))
         redirect_to edit_admin_general_settings_path
+      end
+
+      def dismiss_alert
+        if request.xhr? and params[:alert_id]
+          dismissed = Spree::Config[:dismissed_spree_alerts] || ''
+          Spree::Config.set :dismissed_spree_alerts => dismissed.split(',').push(params[:alert_id]).join(',')
+          filter_dismissed_alerts
+          render :nothing => true
+        end
       end
 
       def clear_cache
